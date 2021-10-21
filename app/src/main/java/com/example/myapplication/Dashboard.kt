1ab2d72dbc4cfce3c5664b5c5a8f889cb52830e5
ZEKHNINE Ayoub <ayoub.zekhnine@efs.sante.fr>
2021-10-21 19:44:54 +0200

    next commit

--- app/src/main/java/com/example/myapplication/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/Dashboard.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +12,17 @@ class Dashboard : AppCompatActivity() {
         setContentView(R.layout.activity_dashboard)
 
         var loginButton  = findViewById<Button>(R.id.loginId)
+        var about = findViewById<ImageView>(R.id.about)
 
         loginButton.setOnClickListener {
 
             intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        about.setOnClickListener{
+
+            intent= Intent(this,AboutActivity::class.java)
             startActivity(intent)
         }
 
